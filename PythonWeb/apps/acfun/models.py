b3b23d0f5154916ca4c473eb132723e324410985
amdone <34444201+amdone@users.noreply.github.com>
2020-12-30 08:39:10 +0800

    admin

--- PythonWeb/apps/acfun/models.py
+++ PythonWeb/apps/acfun/models.py
@@ -1,16 +1,24 @@
 from django.db import models
+import datetime
 
 
 # Create your models here.
 class Acfun(models.Model):
-    acno = models.CharField(db_column='acno', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    # id = models.UUIDField(db_column='id', blank=False, unique=True, null=False, primary_key=True)
+    acno = models.CharField(primary_key=True, db_column='acno', max_length=20, blank=True,
+                            null=False, default='null')  # Field name made lowercase.
     acerid = models.IntegerField(db_column='acerid', blank=True, null=True)
     acname = models.CharField(db_column='acname', max_length=20, blank=True, null=True)  # Field name made lowercase.
     title = models.CharField(db_column='title', max_length=40, blank=True, null=True)  # Field name made lowercase.
     uploadtime = models.IntegerField(db_column='uploadtime', blank=True,
                                      null=True)  # Field name made lowercase.
     cover = models.CharField(db_column='cover', max_length=100, blank=True, null=True)  # Field name made lowercase.
+    remove = models.BooleanField(db_column='remove', blank=True, null=True)
+
+    def __str__(self):
+        return "{}/{}/{}/{}".format(self.acno, self.acname, self.title, self.uploadtime)
 
     class Meta:
         managed = True
         db_table = 'acer'
+        ordering = ['-uploadtime']
