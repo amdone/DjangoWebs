c51f0fe306a3b02230b3681dc2988457ed4f7487
amdone <34444201+amdone@users.noreply.github.com>
2020-12-30 08:46:12 +0800

    Add files via upload

--- PythonWeb/apps/youtube/admin.py
+++ PythonWeb/apps/youtube/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Youtube
+
 
 # Register your models here.
+@admin.register(Youtube)
+class YoutubeAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'channeltitle']
+    # listdisplay设置要显示在列表中的字段（id字段是Django模型的默认主键）
+    list_display = ('videoid', 'title', 'channeltitle', 'description', 'uploadtime')
+
+    # list_per_page设置每页显示多少条记录，默认是100条
+    list_per_page = 50
+
+    # ordering设置默认排序字段，负号表示降序排序
+    ordering = ('-uploadtime',)
+
+    # list_editable 设置默认可编辑字段，在列表里就可以编辑
+    # list_editable = ['title', 'acname']
+
+    # fk_fields 设置显示外键字段
+    # fk_fields = ['category']
--- PythonWeb/apps/youtube/models.py
+++ PythonWeb/apps/youtube/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Youtube(models.Model):
+    videoid = models.CharField(primary_key=True, db_column='videoId', max_length=20, blank=True, null=False,default='null')
+    channelid = models.CharField(db_column='channelId', max_length=20, blank=True, null=True)
+    uploadtime = models.CharField(db_column='upTime', max_length=20, blank=True, null=True)
+    title = models.CharField(db_column='title', max_length=500, blank=True, null=True)
+    video = models.CharField(db_column='video', max_length=60, blank=True, null=True)
+    cover = models.CharField(db_column='cover', max_length=60, blank=True, null=True)
+    description = models.CharField(db_column='description', max_length=500, blank=True, null=True)
+    channeltitle = models.CharField(db_column='channelTitle', max_length=50, blank=True, null=True)
+    userid = models.CharField(db_column='userId', max_length=40, blank=True, null=True)
+    remove = models.BooleanField(db_column='remove', blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'youtube'
